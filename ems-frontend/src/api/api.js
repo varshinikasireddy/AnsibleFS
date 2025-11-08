7fad68abbaf93ec2ba02a22899915291184fd7aa
varshinikasireddy <varshinikasireddy29@gmail.com>
2025-11-08 15:43:30 +0530

    docker files added

--- ems-frontend/src/api/api.js
+++ ems-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 // src/api/api.js
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8083/api';
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:30083/api';
 
 const api = axios.create({
   baseURL: API_BASE,
